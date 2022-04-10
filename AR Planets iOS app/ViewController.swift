9e7b255ac956fb022127cc7ee75db557b06d57c1
lukaszgolojuch <lukasz.golojuch@gmail.com>
2022-04-10 22:26:53 +0200

    Showing planet on screen using Augmented Reality. Changing planets only in code (manually).

--- AR Planets iOS app/ViewController.swift
+++ AR Planets iOS app/ViewController.swift
@@ -20,13 +20,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.showsStatistics = false
         
-        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//
-//        // Set the scene to the view
-//        sceneView.scene = scene
+        let sphere = SCNSphere(radius: 0.2)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "art.scnassets/uranus.jpg")
+        sphere.materials = [material]
+        
+        let node = SCNNode()
+        node.position = SCNVector3(x: 0, y: -0.1, z: -1)
+        node.geometry = sphere
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
